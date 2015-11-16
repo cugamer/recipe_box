00e4ebb60be503d6b52d9ca719fe26121170b210
Jonathon <jonathon.nordquist@gmail.com>
2015-11-16 15:24:22 +0000

    Add users resources to routes.  Add show method to users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root                        "static_pages#homepage"
   
-  get 'signup' =>             "users#new"
+  resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
