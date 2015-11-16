d7a4ae9e4e62b81bbbf9e4505d5a4703eda6512a
Jonathon <jonathon.nordquist@gmail.com>
2015-11-16 18:35:08 +0000

    Create sessions controller, add routes for sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root                         "static_pages#homepage"
+  
+  get     'login'           => 'sessions#new'
+  post    'login'           => 'sessions#create'
+  delete  'logout'          => 'sessions#destroy'
   
   resources :users
   resources :recipies
