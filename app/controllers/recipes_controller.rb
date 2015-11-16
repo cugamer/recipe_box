2de031bad00605040a0139989a33ada366b517de
Jonathon <jonathon.nordquist@gmail.com>
2015-11-16 22:53:47 +0000

    Add require_login method to recipes controller.  Rework create method to create recipe via user.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :require_login
+  
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -8,7 +10,7 @@ class RecipesController < ApplicationController
   end
   
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipe.new(recipe_params)
     if @recipe.save
       redirect_to @recipe
     else
@@ -19,5 +21,12 @@ class RecipesController < ApplicationController
   private
     def recipe_params
       params.require(:recipe).permit(:description, :title, :photo)
+    end
+    
+    def require_login
+      unless logged_in?
+        flash[:error] = "You must be logged in to access this action"
+        redirect_to login_url
+      end
     end
 end
