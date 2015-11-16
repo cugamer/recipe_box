f7be6ec468d910b6565f59769e8998a0e665b651
Jonathon <jonathon.nordquist@gmail.com>
2015-11-16 17:53:07 +0000

    Add no_image file to images.  Add has_attached_file and validation to recipie model.  Add file_field tag to recipies/new view.

--- app/models/recipie.rb
+++ app/models/recipie.rb
@@ -1,4 +1,9 @@
 class Recipie < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 80 }
   validates :description, presence: true, length: { maximum: 1000 }
+  
+  
+  has_attached_file :photo, styles: { medium: "500x500>", thumb: "200x200>" },
+                                      default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
