4ebb1927db886c2612a1cc2d0c239f87a542bd90
Jonathon <jonathon.nordquist@gmail.com>
2015-11-17 21:55:23 +0000

    Add directions to static_pages controller homepage method to get user and last max 12 recipes.  Add display instructions to homepage view.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def homepage
+    if logged_in?
+      @current_user = current_user
+      max_num = [@current_user.recipes.count, 12].min
+      @recipes = @current_user.recipes.last(max_num)
+    end
     
   end
 end
